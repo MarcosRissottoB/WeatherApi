305ffe0254ef15502431eaabb9c5653632cf75d0
marcosirb <marcosrissotto@gmail.com>
2022-11-04 12:24:19 -0300

    feat(redis): add redis
    Add redis, it need fixes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import Fastify from 'fastify'
-import weatherRoutes from './modules/weather/weather.route'
+import Fastify, { FastifyInstance } from 'fastify'
+import { Server, IncomingMessage, ServerResponse } from 'http'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
+import weatherRoutes from './modules/weather/weather.route'
+import fp from 'fastify-plugin'
 
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -10,9 +12,7 @@ const fastify = Fastify({
   logger: true
 })
 
-fastify.register(fastifySwagger)
-
-fastify.register((fastifySwaggerUI), {
+const swaggerOptions = {
   routePrefix: '/docs',
   swagger: {
     info: {
@@ -25,14 +25,42 @@ fastify.register((fastifySwaggerUI), {
     produces: ['application/json']
   },
   exposeRoute: true
-})
+}
 
-fastify.get('/health', function (req, reply) {
-  reply.send({ status: 'Ok' })
-})
+async function redisConnect (fastify, done) {
+  fastify.register(require('@fastify/redis'),
+    {
+      host: '127.0.0.1',
+      port: 6380,
+      namespace: 'hello'
+    })
+  done()
+}
 
-const start = async () => {
-  fastify.register(weatherRoutes, { prefix: 'api/weather' })
+const start = async (): Promise<FastifyInstance<Server, IncomingMessage, ServerResponse>> => {
+  fastify.register(fp((fastify, opts, done) => {
+    fastify.decorate(redisConnect)
+    done()
+  }))
+
+  fastify
+    .register(fastifySwagger)
+    .register(fastifySwaggerUI, swaggerOptions)
+
+  // Routes
+  fastify
+    .register(weatherRoutes, { prefix: 'api/weather' })
+    .get('/api/health', function (req, reply) {
+      reply.send({ status: 'ok' })
+    })
+    .get('/api/cache', function (req, reply) {
+      const { redis } = fastify
+      console.log('redis', redis)
+
+      redis.hello.set(req.body.key, req.body.value, (err) => {
+        reply.send(err || { status: 'ok' })
+      })
+    })
 
   fastify.listen({ port, host: '0.0.0.0' }, function (err, address) {
     if (err) {
@@ -42,4 +70,5 @@ const start = async () => {
     fastify.log.info(`server listening on ${address}`)
   })
 }
+
 start()
